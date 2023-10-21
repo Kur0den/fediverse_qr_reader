b781c489cc937b4c34d084d09d229bfc02e38653
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-10-22 01:13:48 +0900

    インスタンスを記憶するためのインプットボックスを追加

--- main.js
+++ main.js
@@ -1,53 +1,65 @@
-window.onload = (e)=>{
+window.onload = (e) => {
+    let video = document.createElement('video');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    let msg = document.getElementById('msg');
 
-	let video  = document.createElement('video');
-	let canvas = document.getElementById('canvas');
-	let ctx    = canvas.getContext('2d');
-	let msg    = document.getElementById('msg');
+    const userMedia = { video: { facingMode: 'environment' } };
+    navigator.mediaDevices.getUserMedia(userMedia).then((stream) => {
+        video.srcObject = stream;
+        video.setAttribute('playsinline', true);
+        video.play();
+        startTick();
+    });
 
-	const userMedia = {video: {facingMode: 'environment'}};
-	navigator.mediaDevices.getUserMedia(userMedia).then((stream)=>{
-		video.srcObject = stream;
-		video.setAttribute('playsinline', true);
-		video.play();
-		startTick();
-	});
-
-	function startTick(){
-		msg.innerText = 'Loading video...';
-		if(video.readyState === video.HAVE_ENOUGH_DATA){
-			canvas.height = video.videoHeight;
-			canvas.width = video.videoWidth;
-			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-			let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
-			let code = jsQR(img.data, img.width, img.height, {inversionAttempts: 'dontInvert'});
-			if(code){
-				drawRect(code.location);// Rect
-				msg.innerText = code.data;// Data
-                if(code.data.startsWith('https://')){
-                    console.log(true)
-                    location.href = code.data
+    function startTick() {
+        msg.innerText = 'Loading video...';
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            let code = jsQR(img.data, img.width, img.height, {
+                inversionAttempts: 'dontInvert',
+            });
+            if (code) {
+                drawRect(code.location); // Rect
+                msg.innerText = code.data; // Data
+                if (code.data.startsWith('https://')) {
+                    console.log(true);
+                    location.href = code.data;
                 }
-			}else{
-				msg.innerText = 'Detecting QR-Code...';
-			}
-		}
-		setTimeout(startTick, 100);
-	}
+            } else {
+                msg.innerText = 'Detecting QR-Code...';
+            }
+        }
+        setTimeout(startTick, 100);
+    }
 
-	function drawRect(location){
-		drawLine(location.topLeftCorner,     location.topRightCorner);
-		drawLine(location.topRightCorner,    location.bottomRightCorner);
-		drawLine(location.bottomRightCorner, location.bottomLeftCorner);
-		drawLine(location.bottomLeftCorner,  location.topLeftCorner);
-	}
+    function drawRect(location) {
+        drawLine(location.topLeftCorner, location.topRightCorner);
+        drawLine(location.topRightCorner, location.bottomRightCorner);
+        drawLine(location.bottomRightCorner, location.bottomLeftCorner);
+        drawLine(location.bottomLeftCorner, location.topLeftCorner);
+    }
 
-	function drawLine(begin, end){
-		ctx.lineWidth = 4;
-		ctx.strokeStyle = '#FF3B58';
-		ctx.beginPath();
-		ctx.moveTo(begin.x, begin.y);
-		ctx.lineTo(end.x, end.y);
-		ctx.stroke();
-	}
+    function drawLine(begin, end) {
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = '#FF3B58';
+        ctx.beginPath();
+        ctx.moveTo(begin.x, begin.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+    }
+};
+
+const button = document.getElementById('memoryzeButton');
+const instance = document.getElementById('instanceDomain');
+
+button.addEventListener('click', instance_store);
+
+function instance_store() {
+    if (navigator.cookieEnabled) {
+        document.cookie = 'instance=' + encodeURIComponent(instance.value)
+    }
 }
