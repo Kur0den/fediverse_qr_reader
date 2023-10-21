d88c5701103c09bee29cdcf24953130f428bd5e7
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-10-22 00:37:41 +0900

    ダブルクォーテーションをシングルクォーテーションに変更
    宗教上の理由的な
    こっちのほうが好き

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 window.onload = (e)=>{
 
-	let video  = document.createElement("video");
-	let canvas = document.getElementById("canvas");
-	let ctx    = canvas.getContext("2d");
-	let msg    = document.getElementById("msg");
+	let video  = document.createElement('video');
+	let canvas = document.getElementById('canvas');
+	let ctx    = canvas.getContext('2d');
+	let msg    = document.getElementById('msg');
 
-	const userMedia = {video: {facingMode: "environment"}};
+	const userMedia = {video: {facingMode: 'environment'}};
 	navigator.mediaDevices.getUserMedia(userMedia).then((stream)=>{
 		video.srcObject = stream;
-		video.setAttribute("playsinline", true);
+		video.setAttribute('playsinline', true);
 		video.play();
 		startTick();
 	});
 
 	function startTick(){
-		msg.innerText = "Loading video...";
+		msg.innerText = 'Loading video...';
 		if(video.readyState === video.HAVE_ENOUGH_DATA){
 			canvas.height = video.videoHeight;
 			canvas.width = video.videoWidth;
 			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 			let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
-			let code = jsQR(img.data, img.width, img.height, {inversionAttempts: "dontInvert"});
+			let code = jsQR(img.data, img.width, img.height, {inversionAttempts: 'dontInvert'});
 			if(code){
 				drawRect(code.location);// Rect
 				msg.innerText = code.data;// Data
@@ -29,10 +29,10 @@ window.onload = (e)=>{
                     location.href = code.data
                 }
 			}else{
-				msg.innerText = "Detecting QR-Code...";
+				msg.innerText = 'Detecting QR-Code...';
 			}
 		}
-		setTimeout(startTick, 250);
+		setTimeout(startTick, 100);
 	}
 
 	function drawRect(location){
@@ -44,7 +44,7 @@ window.onload = (e)=>{
 
 	function drawLine(begin, end){
 		ctx.lineWidth = 4;
-		ctx.strokeStyle = "#FF3B58";
+		ctx.strokeStyle = '#FF3B58';
 		ctx.beginPath();
 		ctx.moveTo(begin.x, begin.y);
 		ctx.lineTo(end.x, end.y);
